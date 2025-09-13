ea063d075ceb9c9da3bf4ef04a3bd8f74a0ea6db
EmaaZmaker <emaazbinammar@gmail.com>
2025-09-13 10:28:28 +0400

    cool

---
+++ Activity1.py
@@ -0,0 +1,12 @@
+import tkinter
+from tkinter import *
+
+#creating window
+
+new_window=Tk()
+
+#setting the windows,title and geometry
+
+new_window.title("Test")
+new_window.geometry("400x300")
+new_window.mainloop()
