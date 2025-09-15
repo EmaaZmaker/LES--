1b6e9f9ca3963069475e3471a72877e460b0f0d6
EmaaZmaker <emaazbinammar@gmail.com>
2025-09-15 13:28:31 +0400

    was pretty hard

---
+++ apy2.py
@@ -0,0 +1,12 @@
+import math
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self):
+        return math.pi * self.radius * self.radius
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+r = float(input("Enter the radius of the circle: "))
+my_circle = Circle(r)
+print("Area of the circle is:", my_circle.area())
+print("Perimeter of the circle is:", my_circle.perimeter())
